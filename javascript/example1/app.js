f7f638e189249659e7149928d77328a2741026b3
KoranchirathKrishnaprasad <krishnaprasadkoranchirath92@gmail.com>
2024-05-20 23:49:53 +0530

    git

--- javascript/example1/app.js
+++ javascript/example1/app.js
@@ -1,1 +1,19 @@
 console.log("hii js!")
+let color ='red';
+if(color =='red') {
+console.log("Stop");
+} else if(color =='yellow') {
+    console.log("Wait");
+}else if(color =='Green') {
+    console.log("Go");
+}
+let size = prompt("Enter size");
+if(size == 'XL') {
+    alert('Price is 250');
+} else if(size == 'L') {
+    alert('Price is 200');
+} else if(size == 'M') {
+    alert('Price is 150');
+} else if(size == 'S') {
+    alert('Price is 100');
+}

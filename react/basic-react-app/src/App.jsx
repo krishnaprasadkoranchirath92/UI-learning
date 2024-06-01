35fa661176e6024feb3b5184306cb6fdfde3171b
KoranchirathKrishnaprasad <krishnaprasadkoranchirath92@gmail.com>
2024-06-02 01:58:51 +0530

    react 1

---
+++ react/basic-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function Title() {
+  return <h1>Hello World!</h1>
+}
+function App() {
+
+  return (
+    <>
+      <Title />
+      <button>Hello!</button>
+    </>
+  )
+}
+
+export default App
